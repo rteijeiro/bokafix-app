162ad61ce8bc9d67ce9ce55f1c08a2562d947386
Ruben Teijeiro <ruben@youpal.se>
2022-10-11 14:01:08 +0200

    Checks palindromes in sentende.

--- lib/palindrome.dart
+++ lib/palindrome.dart
@@ -1,3 +1,21 @@
-bool isPanlindrome(String sentence) {
-  return sentence == sentence.split('').reversed.join();
+bool isPanlindrome(String word) {
+  return word == word.split('').reversed.join();
+}
+
+bool findPalindromes(String sentence) {
+  bool palindomeFound = false;
+
+  List<String> wordList = sentence.split(" ");
+  for (final word in wordList) {
+    if (isPanlindrome(word)) {
+      palindomeFound = true;
+      break;
+    }
+  }
+
+  if (palindomeFound) {
+    print("The sentence contains a palindrome.");
+  } else {
+    print("The sentence does not contain a palindrome.");
+  }
 }
