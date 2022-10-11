892028c5c8acb7f79e9b0d15ae7937ed4912a9ec
Ruben Teijeiro <ruben@youpal.se>
2022-10-11 12:46:55 +0200

    Implements home teleport.

--- lib/main.dart
+++ lib/main.dart
@@ -38,11 +38,13 @@ class _MyAppState extends State<MyApp> {
 
   void _teleportHome() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+      mapController.animateCamera(CameraUpdate.newCameraPosition(
+        CameraPosition(
+          bearing: 0,
+          target: LatLng(59.3293, 18.0686),
+          zoom: 12.0,
+        ),
+      ));
     });
   }
 
